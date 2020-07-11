886420ad772b01b0377ee8af885f9eaec0b42561
João Henrique <joao.henrique_15@hotmail.com>
2020-07-11 15:07:17 -0300

    Aula 13 e 14 - Refatorando as entidades e implementando principios SOLID

---
+++ FaustinoStore.Domain/StoreContext/Entities/Delivery.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Faustinotore.Domain.StoreContext.Enums;
+
+namespace FaustinoStore.Domain.StoreContext.Entities
+{
+  public class Delivery
+  {
+    public Delivery(DateTime estimateDeliveryDate)
+    {
+      CreateDate = DateTime.Now;
+      EstimateDeliveryDate = estimateDeliveryDate;
+      Status = EDeliveryStatus.Waiting;
+    }
+
+    public DateTime CreateDate { get; private set; }
+    public DateTime EstimateDeliveryDate { get; private set; }
+    public EDeliveryStatus Status { get; private set; }
+  }
+}
--- FaustinoStore.Domain/StoreContext/OrderItem.cs
+++
@@ -1,9 +0,0 @@
-namespace FaustinoStore.Domain.StoreContext
-{
-  public class OrderItem
-  {
-    public Product Product { get; set; }
-    public string Quantity { get; set; }
-    public string Price { get; set; }
-  }
-}
