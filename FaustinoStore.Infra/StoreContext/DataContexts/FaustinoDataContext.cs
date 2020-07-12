5abccfb9df78f9530fefbf8a8bee055c69e9b254
João Henrique <joao.henrique_15@hotmail.com>
2020-07-12 13:22:22 -0300

    Aula 42 - Criando o Repositório

--- FaustinoStore.Infra/StoreContext/DataContexts/FaustinoDataContext.cs
+++ FaustinoStore.Infra/StoreContext/DataContexts/FaustinoDataContext.cs
@@ -5,20 +5,20 @@ using FaustinoStore.Shared;
 
 namespace FaustinoStore.Infra.DataContexts
 {
-    public class FaustinoDataContext : IDisposable
+  public class FaustinoDataContext : IDisposable
+  {
+    public SqlConnection Connection { get; set; }
+
+    public FaustinoDataContext()
     {
-        public SqlConnection Connection { get; set; }
-
-        public FaustinoDataContext()
-        {
-            Connection = new SqlConnection(Settings.ConnectionString);
-            Connection.Open();
-        }
-
-        public void Dispose()
-        {
-            if (Connection.State != ConnectionState.Closed)
-                Connection.Close();
-        }
+      Connection = new SqlConnection(Settings.ConnectionString);
+      Connection.Open();
     }
+
+    public void Dispose()
+    {
+      if (Connection.State != ConnectionState.Closed)
+        Connection.Close();
+    }
+  }
 }
