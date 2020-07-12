c16672c61639d729c5fbb0e26b9c09b8bb123a64
João Henrique <joao.henrique_15@hotmail.com>
2020-07-12 13:10:51 -0300

    Aula 41 - Criando o DataContext

---
+++ FaustinoStore.Infra/StoreContext/DataContexts/FaustinoDataContext.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using FaustinoStore.Shared;
+
+namespace FaustinoStore.Infra.DataContexts
+{
+    public class FaustinoDataContext : IDisposable
+    {
+        public SqlConnection Connection { get; set; }
+
+        public FaustinoDataContext()
+        {
+            Connection = new SqlConnection(Settings.ConnectionString);
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (Connection.State != ConnectionState.Closed)
+                Connection.Close();
+        }
+    }
+}
---
+++ FaustinoStore.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace FaustinoStore.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FaustinoStore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
