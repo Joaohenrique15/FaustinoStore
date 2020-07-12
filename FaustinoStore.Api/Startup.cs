5e4d8e4a3ec1fa6ae24cc5ca8e87d46b4e977693
João Henrique <joao.henrique_15@hotmail.com>
2020-07-12 16:41:43 -0300

    Finalização e alguns ajuste na execução do projeto

--- FaustinoStore.Api/Startup.cs
+++ FaustinoStore.Api/Startup.cs
@@ -1,11 +1,14 @@
+using System.IO;
 using FaustinoStore.Domain.StoreContext.Handlers;
 using FaustinoStore.Domain.StoreContext.Repositories;
 using FaustinoStore.Domain.StoreContext.Services;
 using FaustinoStore.Infra.DataContexts;
 using FaustinoStore.Infra.StoreContext.Repositories;
 using FaustinoStore.Infra.StoreContext.Services;
+using FaustinoStore.Shared;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
@@ -14,9 +17,16 @@ namespace FaustinoStore.Api
 {
   public class Startup
   {
+    public static IConfiguration Configuration { get; set; }
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc();
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+      Configuration = builder.Build();
+
+      services.AddControllers();
 
       services.AddResponseCompression();
 
@@ -30,6 +40,7 @@ namespace FaustinoStore.Api
         x.SwaggerDoc("v1", new OpenApiInfo { Title = "FaustinoStore", Version = "v1" });
       });
 
+      Settings.ConnectionString = $"{Configuration["connectionString"]}";
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -37,7 +48,12 @@ namespace FaustinoStore.Api
       if (env.IsDevelopment())
         app.UseDeveloperExceptionPage();
 
-      app.UseMvc();
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+        {
+          endpoints.MapControllers();
+        });
+
       app.UseResponseCompression();
 
       app.UseSwagger();
