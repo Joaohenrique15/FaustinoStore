6bf132e3454addccbfea590c1262942d9ad58b5c
João Henrique <joao.henrique_15@hotmail.com>
2020-07-12 10:43:11 -0300

    Aula 31 - Testando Commands

---
+++ FaustinoStore.Tests/Commands/CreateCustomerCommandTests.cs
@@ -0,0 +1,22 @@
+using FaustinoStore.Domain.StoreContext.CustomerCommands.Inputs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaltaStore.Tests
+{
+  [TestClass]
+  public class CreateCustomerCommandTests
+  {
+    [TestMethod]
+    public void ShouldValidateWhenCommandIsValid()
+    {
+      var command = new CreateCustomerCommand();
+      command.FirstName = "João";
+      command.LastName = "Faustino";
+      command.Document = "12345678910";
+      command.Email = "joao.henrique_15@hotmail.com";
+      command.Phone = "1198654321";
+
+      Assert.AreEqual(true, command.Valid());
+    }
+  }
+}
